5081ff528c082b13cdc132f37e454f7ec71b8e09
Angelo Saraceno <asara019@fiu.edu>
2016-12-26 17:30:09 -0500

    ex1 b4 sd

---
+++ exercise1/ex1.py
@@ -0,0 +1,8 @@
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "I like typing this."
+print "This is fun"
+print "Yay! Printing."
+print "I'd rather much you not"
+print 'I "said" to not touch this'
