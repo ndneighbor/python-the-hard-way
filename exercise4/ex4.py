4090b2e14e09fdacba26dd03cc15716136121496
Angelo Saraceno <asara019@fiu.edu>
2017-01-02 22:14:19 -0500

    4 finish

--- exercise4/ex4.py
+++ exercise4/ex4.py
@@ -11,3 +11,6 @@ average_passengers_per_car = passengers / cars_driven
 print "There are", cars, "cars availible."
 print "There are only", drivers, "drivers availible."
 print "There will be", cars_not_driven, "empty cars today."
+print "We can transport", carpool_capacity, "people today."
+print "We have", passengers, "to carpool today."
+print "We need to put about", average_passengers_per_car, "in each car."
