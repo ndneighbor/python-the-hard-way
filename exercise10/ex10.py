7764e029df55c1aefb1796c8fd431298eae8c356
Angelo Saraceno <asara019@fiu.edu>
2017-01-04 23:43:20 -0500

    ex10,11

---
+++ exercise10/ex10.py
@@ -0,0 +1,15 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line"
+backslash_cat = "Im a \\ a \\ cat"
+
+fat_cat = """
+I'll do a list
+\t* Cat food
+\t* Fishes 
+\t* Catnip\nt* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
---
+++ exercise11/ex11.py
@@ -0,0 +1,9 @@
+print "How old are you?",
+age = raw_input()
+print "",
+height = raw_input()
+print "",
+weight = raw_input()
+
+print "So you're %r old, %r tall and %r heavy." % (
+    age, height, weight)
