02f44aea9c5e36253fab36b2993105ff604076b5
Angelo Saraceno <asara019@fiu.edu>
2017-01-30 19:23:04 -0500

    added it all

--- exercise16/ex16.py
+++ exercise16/ex16.py
@@ -24,3 +24,10 @@ print "I'm going to write these to the file."
 
 target.write(line1)
 target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+target.write("\n")
+
+print "And finally, we close it."
+target.close()
